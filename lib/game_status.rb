96436ff55d5cf037e84b83d0deb0b97f10720521
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2021-03-11 23:50:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,92 +6,33 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+#TRUE - position has been taken
 
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]]
 
+# find position_taken = WIN_COMBINATIONS
 
 def won?(board)
-  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    return false
-  # elsif full?(board)
-  #   return false
-  elsif draw?(board)
-    return false
-  else position_taken?(board[])
-    WIN_COMBINATIONS.each  do |combo|
-      combo == board[] && board[] = "X" || board[] = "O"
+  if board [0..8]
+   return false
+  else position_taken?(board, 0..8)
+    WIN_COMBINATIONS[0..8].each  do |combo|
+      combo == board [][] && board[][] = "X" || board[][] = "O"
+    return
+    
+      case win_location
+      when WIN_COMBINATIONS[0][0]
+        return "Top row win"
+        
+      when WIN_COMBINATIONS[0][1]
+        return "Middle row win"
+      
+else
+    # stuff here
+end
+
+
     end
   end  
 end
-
-def draw?(board, index)
-  if won?(board)
-    return false
-  else position_taken?(board, index)
-    return false
-  end
-end
-
-  # describe '#draw?' do
-  #   it 'returns true for a draw' do
-  #     board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-
-  #     expect(draw?(board)).to be_truthy
-  #   end
-
-  #   it 'returns false for an in-progress game' do
-  #     board = ["X", " ", "X", " ", "X", " ", "O", "O", "X"]
-
-  #     expect(draw?(board)).to be_falsey
-  #   end
-  # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def full?(board)
-#   if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-#     return false
-#   else position_taken?(board, index)
-#   end  
-# end
 
